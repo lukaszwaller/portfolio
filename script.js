aae96013b10f6da1d39813b209174fed09846a79
lukaszwaller <admin@lukas-zwaller.com>
2024-07-04 23:51:28 +0200

    dark mode for pullover

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
   function setTheme(theme) {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
+
+    if (theme === "dark") {
+      document.querySelector(".profile-pic").src = "assets/me-light.png";
+    } else {
+      document.querySelector(".profile-pic").src = "assets/me-dark.png";
+    }
   }
 
   const currentTheme = localStorage.getItem("theme");
@@ -15,8 +21,16 @@ document.addEventListener("DOMContentLoaded", function () {
   if (currentTheme) {
     document.documentElement.setAttribute("data-theme", currentTheme);
     toggleSwitch.checked = currentTheme === "dark";
+
+    if (currentTheme === "dark") {
+      document.querySelector(".profile-pic").src = "assets/me-light.png";
+    } else {
+      document.querySelector(".profile-pic").src = "assets/me-dark.png";
+    }
   } else {
-    toggleSwitch.checked = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    setTheme(toggleSwitch.checked ? "dark" : "light");
+    const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    toggleSwitch.checked = prefersDark;
+    setTheme(prefersDark ? "dark" : "light");
   }
 });
+
